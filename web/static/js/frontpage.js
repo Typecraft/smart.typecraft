1740489a0be96b0eceb02ba5c926615eeeb05e51
Tormod Haugland <tormod.haugland@gmail.com>
2017-01-24 09:55:50 +0100

    Added more css and js for frontpage

--- web/static/js/frontpage.js
+++ web/static/js/frontpage.js
@@ -1,6 +1,20 @@
-
-$(document).ready(function(){
+/* global window, document, $ */
+$(document).ready(function() {
   $(".section__top").parallax({
     imageSrc: "/static/img/pexels-photo-132650.jpeg"
-  })
+  });
+
+  $(".section__numbers").parallax({
+    imageSrc: "/static/img/pexels-photo-94654.jpeg"
+  });
+
+  $("[data-countup]").countup({
+    startVal: 10,  // parseInt($(this).data("start"), 10),
+    endVal: 1000  // parseInt($(this).data("end"), 10)
+  });
+
+  $("a.scrollto").click(function(event) {
+    event.preventDefault();
+    $(window).scrollTo($($(this).attr('href')), 1000);
+  });
 });
